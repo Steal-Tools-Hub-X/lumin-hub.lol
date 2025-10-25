37524f0bdc07cdce6dcbe4952874d77238f7d28d
Steal-Tools-Hub-X <infofacts56@gmail.com>
2025-10-25 19:07:01 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.exe/refs/heads/main/Loader.lua",true))()
+loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.exe/refs/heads/main/Loader.lua",true))()",true))()
